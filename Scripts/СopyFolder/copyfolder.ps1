06a699612263a555019781005f66d3ca1b357a79
r.mingardo <rino@mingardo.ru>
2025-10-31 22:19:11 +0300

    feat: create copyfolder.ps1

---
+++ Scripts/СopyFolder/copyfolder.ps1
@@ -0,0 +1,18 @@
+# Путь к исходной папке
+$source = "C:\Git\Obsidian"
+
+# Путь к целевой папке
+$destination = "C:\Users\Rinov\iCloudDrive\Obsidian"
+
+# Создаём целевую папку, если её нет
+if (-not (Test-Path -Path $destination)) {
+    New-Item -ItemType Directory -Path $destination
+}
+
+# Копируем содержимое (все подпапки и файлы)
+Copy-Item -Path "$source\*" -Destination $destination -Recurse -Force
+
+# Можно добавить запись в лог
+$logFile = "C:\tmp\backup_log.txt"
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+"$timestamp - Backup completed from $source to $destination" | Out-File -FilePath $logFile -Append
